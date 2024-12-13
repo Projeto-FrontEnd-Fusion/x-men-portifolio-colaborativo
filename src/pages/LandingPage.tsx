2816c0ceded660c173c219be727b28a8f50dd14b
Ana Ferreira <anapaulaferreira0309@gmail.com>
2024-12-13 17:29:12 -0300

    feat: import about section component to LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,13 @@
+import { AboutSection } from "../components/AboutSection/AboutSection";
+
 export default function LandingPage() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline text-white">
         Portfólio Colaborativo
       </h1>
+      <AboutSection></AboutSection>
     </main>
+
   );
 };
