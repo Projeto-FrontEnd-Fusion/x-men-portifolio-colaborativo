5ff45f976f662622b9ca0960ac55bf8445692be0
Ana Ferreira <anapaulaferreira0309@gmail.com>
2024-12-21 13:28:13 -0300

    feat: create LevelUpSection and import into LandingPage

--- src/components/LevelUpSection.tsx
+++ src/components/LevelUpSection.tsx
@@ -1,7 +1,18 @@
-export default function LevelUpSection() {
+export function LevelUpSection() {
   return (
-    <section>
+    <section id="levelUpSection">
+      <div>
+        <h2 className="text-2xl lg:text-5xl font-medium">Venha subir de nível com a gente!</h2>
+        <p className="text-base lg:text-lg">Vamos transformar o aprendizado de desenvolvimento com inovação e divertimento. Assim como disse um grande sábio “A caminhada pode ser longa, mas a vista vale a pena”.</p>
 
+        <button type="button">Faça parte</button>
+      </div>
+
+      <div>
+        <div className="socials">
+          <a href="" target="_blank" rel="noopener noreferrer" aria-label="Visite meu perfil no LinkedIn">LinkedIn</a>
+        </div>
+      </div>
     </section>
   );
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,13 @@
+import { LevelUpSection } from '../components/LevelUpSection';
+
 export default function LandingPage() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline text-white">
         Portfólio Colaborativo
       </h1>
+
+      <LevelUpSection />
     </main>
   );
 };
