0717570d959b46d94b12a312c41fe16128306dc9
Ana Ferreira <anapaulaferreira0309@gmail.com>
2024-12-30 20:14:47 -0300

    feat: import level-up section and footer components to LandindPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,13 +1,15 @@
+import { Footer } from '../components/Footer';
 import { LevelUpSection } from '../components/LevelUpSection';
 
 export default function LandingPage() {
   return (
-    <main>
+    <main id='main'>
       <h1 className="text-3xl font-bold underline text-white">
         Portfólio Colaborativo
       </h1>
 
       <LevelUpSection />
+      <Footer />
     </main>
   );
 };
