8483e60261966940fd1460b24066d3fa15a15671
Ana Ferreira <anapaulaferreira0309@gmail.com>
2025-01-11 12:00:10 -0300

    refactor: add LevelUp component into the LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,13 @@
 
 import Hero from "../components/hero/hero";
+import LevelUp from "../components/LevelUp/LevelUp";
 
 export default function App() {
   return (
     <main>
 
       <Hero />
+      <LevelUp />
 
     </main>
   )
