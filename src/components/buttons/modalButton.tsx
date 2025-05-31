487ca38f13705edf9896b138821b014cd7f303a2
Lostleleco <alex_soares_oliveira@hotmail.com>
2025-05-31 11:19:55 -0300

    bugFix: Modal Padding

--- src/components/buttons/modalButton.tsx
+++ src/components/buttons/modalButton.tsx
@@ -12,7 +12,7 @@ const ModalButton: React.FC<ModalButtonProps> = ({ onClick, children, className,
         <button
             onClick={onClick}
             type={type} // Usando a propriedade type
-            className={`w-72 h-16 rounded-lg bg-[#28eeed]  text-[#022440] font-bold leading-8 border border-[#EEEEEE] ${className || ""} 
+            className={`w-72 h-16 rounded-lg bg-[#28eeed] text-[#022440] font-bold leading-8 border border-[#EEEEEE] ${className || ""} 
           sm:w-44 sm:justify-start sm:h-12 md:w-72 md:h-16`} // Responsividade: menor tamanho em dispositivos móveis (sm), tamanho normal em dispositivos maiores (md)
         >
             {children}
