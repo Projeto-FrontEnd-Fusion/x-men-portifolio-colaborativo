7a662c76226de5b3dc5f7a47eda6a25e070e36aa
Luis <siul.l@hotmail.com>
2025-06-13 13:29:44 -0300

    feat: criação e correção de bugs da parte next lv

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,12 +1,11 @@
-
 import Hero from "../components/hero/hero";
+import { NextLevel } from "../components/nextLevel/nextLevel";
 
 export default function App() {
   return (
     <main>
-
       <Hero />
-
+      <NextLevel />
     </main>
-  )
+  );
 }
