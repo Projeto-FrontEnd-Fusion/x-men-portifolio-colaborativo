922a9a8bdc49beae53663426d34415f4e7d65656
Luis <siul.l@hotmail.com>
2025-06-17 21:34:44 -0300

    feat: arquivos .ts apagados

--- src/App.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx, Fragment as _Fragment } from "react/jsx-runtime";
-import LandingPage from "./pages/LandingPage";
-export default function App() {
-    return (_jsx(_Fragment, { children: _jsx(LandingPage, {}) }));
-}
