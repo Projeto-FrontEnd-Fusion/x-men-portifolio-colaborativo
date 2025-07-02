d91b2d7d720b547e063fc9b151d02eadf1b1f7b1
devporpino <isaacgithub14@gmail.com>
2025-07-02 19:32:27 -0300

    Adicionando o footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <footer>
+      <div>
+        <p>Frontend Fusion • 2023 - 2025</p>
+      </div>
+
+      <div>
+        <span>Tecnologia</span>
+        <span>Inovação</span>
+        <span>Interação</span>
+      </div>
+
+      <div>
+        <button onClick={scrollToTop}>
+          Voltar ao topo
+        </button>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
