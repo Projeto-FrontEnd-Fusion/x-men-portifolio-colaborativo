d56b5fdd5afa91c45e7af958af45320a608ebd16
Luis <siul.l@hotmail.com>
2025-07-15 13:08:44 -0300

    feat: ajuste do botão que estava sumindo

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaArrowCircleUp } from "react-icons/fa";
+import arrow from "../../assets/arrow.png";
 
 export const Footer: React.FC = () => {
   const scrollToTop = () => {
@@ -10,34 +10,34 @@ export const Footer: React.FC = () => {
   };
 
   return (
-    <footer className="flex flex-row bg-[#2B3A4F] lg:bg-[#022440] w-full justify-center items-center py-8 lg:py-12  px-4">
-      <div className="flex w-full gap-6 flex-col lg:flex-row justify-evenly lg:py-2 lg:px-8 ">
+    <footer className="flex flex-row bg-[#2B3A4F] md:bg-[#022440] w-full justify-center items-center py-8 lg:py-12 gap-4  px-4">
+      <div className="flex w-full gap-6 flex-col md:flex-row justify-evenly lg:py-2 lg:px-8 ">
         <div className="text-[#EEEEEEBF]">
           <p>Frontend Fusion • 2023 - 2025</p>
         </div>
 
-        <div className="flex flex-row text-[#EEEEEEBF] lg:gap-8 gap-5">
+        <div className="flex flex-row flex-wrap text-[#EEEEEEBF] lg:gap-8 gap-5">
           <span>Tecnologia</span>
           <span>Inovação</span>
           <span>Interação</span>
         </div>
 
-        <div className="flex-row text-[#EEEEEEBF]">
+        <div className="hidden md:block flex-row text-[#EEEEEEBF]">
           <button
-            className="hidden flex-col-reverse lg:flex-row justify-center gap-1 items-center"
+            className=" flex flex-row justify-center gap-2 items-center"
             onClick={scrollToTop}
           >
-            Voltar ao topo <FaArrowCircleUp size={24} />
+            Voltar ao topo <img src={arrow} />
           </button>
         </div>
       </div>
       <div>
-        <div className="lg:hidden px-3 flex w-[130px] h-[48px] text-[#EEEEEEBF]">
+        <div className="block md:hidden px-3 w-[130px] h-[48px] text-[#EEEEEEBF]">
           <button
             className="text-ts flex flex-col-reverse w-full  lg:flex-row justify-center gap-1 items-start"
             onClick={scrollToTop}
           >
-            Voltar ao topo <FaArrowCircleUp size={24} />
+            Voltar ao topo <img src={arrow} />
           </button>
         </div>
       </div>
