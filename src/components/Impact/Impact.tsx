6cf94f30a23bcff08afebd49c6343b6adde5d65c
Lostleleco <alex_soares_oliveira@hotmail.com>
2025-07-20 10:32:14 -0300

    Fix: Styles Cards

--- src/components/Impact/Impact.tsx
+++ src/components/Impact/Impact.tsx
@@ -23,26 +23,26 @@ function Impact() {
 
   return (
     <section className="text-white  w-full py-12 bg-[#022440] ">
-      <ul className="flex flex-col semi:flex-row semi:flex-wrap custom:flex-nowrap gap-6 mx-auto max-w-7xl px-4">
+      <ul className="flex flex-col custom:flex-row custom:flex-wrap custom:gap-6 gap-4 mx-auto max-w-7xl px-4">
         {impacts.map((impact, index) => (
           <li
             key={index}
-            className="min-w-[250px] w-full semi:w-[calc(50%-0.75rem)] custom:w-[calc(33.333%-1rem)]"
+            className="min-w-[250px] w-full custom:w-[calc(33.333%-1rem)]"
           >
-            <article className="min-w-[250px] w-full sm:bg-[#2b3a4f] bg-transparent p-6 rounded-2xl transition duration-300 text-left flex flex-col items-start sm:items-center">
+            <article className="min-w-[250px] w-full custom:bg-[#2b3a4f] bg-transparent p-6 rounded-2xl transition duration-300 text-left flex flex-col items-start custom:items-center">
               <img
                 src={Trofeu}
-                className="w-6 mb-2 sm:hidden"
+                className="w-6 mb-2 custom:hidden"
                 alt="Imagem de um troféu"
               />
-              <div className="hidden sm:flex items-center justify-center gap-2 mb-4">
+              <div className="hidden custom:flex items-center justify-center gap-2 mb-4">
                 <img src={Vector} className="w-9" alt="Imagem de um raio" />
-                <h3 className="text-2xl  font-bold mb-0">{impact.title}</h3>
+                <h3 className="text-2xl font-bold mb-0">{impact.title}</h3>
               </div>
-              <h3 className="text-3xl font-medium mb-2 block sm:hidden">
+              <h3 className="text-3xl font-medium mb-2 block custom:hidden">
                 {impact.title}
               </h3>
-              <p className="text-gray-300  justify-start">
+              <p className="text-gray-300 justify-start">
                 {impact.description}
               </p>
             </article>
